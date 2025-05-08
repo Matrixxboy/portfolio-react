ab17afaf818d881f39b5b437ece04048f45d106d
Matrixxboy <matrix.utsav.lankapati@gmail.com>
2025-05-08 16:59:45 +0530

    page copied

---
+++ frontend/src/main/pages/aboutpage/Aboutpage.jsx
@@ -0,0 +1,6 @@
+<div class="min-h-screen flex items-center justify-center  text-white">
+    <div class="text-center">
+        <h1 class="text-4xl font-bold mb-4">About Page</h1>
+        <p class="text-lg text-gray-300">Coming Soon... Stay Tuned!</p>
+    </div>
+</div>
